64574155dcbe890a9f81f8e408530adbaac9e6ff
ALlC84 <alejandro22cuba@hotmail.com>
2020-05-02 18:20:44 +0200

    En este punto recogemos las datos del CMS segun lo esperado en cada caso y tod funciona OK, version 1.0.0 de la web.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 
-const dataButtonSelect = require('./public/data/data-button-select.json')
+const dataButtonSelect = require('./public/settings/data-button-select.json')
 // const dataSidebarConfig = require('./public/data/data-sidebar-config.json')
 
 module.exports = {
@@ -13,7 +13,7 @@ module.exports = {
          {
             text: 'Notas',
             ariaLabel: 'Menu de notas',
-            items: dataButtonSelect
+            items: dataButtonSelect.listlinks
          },
 			{ text: 'About', link: 'https://cv.alejandrollorente.com' },
       ],
